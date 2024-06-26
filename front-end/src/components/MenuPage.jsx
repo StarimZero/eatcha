186be944f67ca0e2a4dbeb3e3debffba2fde7a81
Seoptrike <dlstjq977@naver.com>
2024-06-26 20:51:50 +0900

    뱃지

--- front-end/src/components/MenuPage.jsx
+++ front-end/src/components/MenuPage.jsx
@@ -29,11 +29,11 @@ const MenuPage = () => {
                             <Navbar.Brand href="/"><img src="../image/mainlogo2.jpg" width={"5%"} /></Navbar.Brand>
                             <Navbar.Toggle aria-controls="navbarScroll" />
                             <Navbar.Collapse id="navbarScroll">
-                                <Nav.Link href="/restaurant/list">ADMIN메뉴==========</Nav.Link>
-                                <Nav.Link href="/restaurant/list">전체식당리스트</Nav.Link>
-                                <Nav.Link href="/member/list.json"><Badge>전체회원리스트</Badge></Nav.Link>
-                                <Nav.Link href="/friends/admin/list.json"><Badge bg='dark'>전체친구리스트</Badge></Nav.Link>
-                                <Nav.Link href="/badge/create"><Badge bg='success'>뱃지만들기</Badge></Nav.Link>
+                                <Nav.Link href="/restaurant/insert"><Badge bg='warning' className='me-2'>식당등록하기</Badge></Nav.Link>
+                                <Nav.Link href="/restaurant/list"><Badge bg='danger' className='me-2'>전체식당리스트</Badge></Nav.Link>
+                                <Nav.Link href="/member/list.json"><Badge className='me-2'>전체회원리스트</Badge></Nav.Link>
+                                <Nav.Link href="/friends/admin/list.json"><Badge bg='dark' className='me-2'>전체친구리스트</Badge></Nav.Link>
+                                <Nav.Link href="/badge/create"><Badge bg='success' className='me-2'>뱃지만들기</Badge></Nav.Link>
                                 <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
                                 </Nav>
                                 <Nav.Link href="#action2" style={{ fontSize: "25px" }}><CiBellOn />　</Nav.Link>
@@ -73,6 +73,7 @@ const MenuPage = () => {
                             <Route path='/member/*' element={<MemberRouter />} />
                             <Route path='/friends/*' element={<FriendsRouter />} />
                             <Route path='/badge/*' element={<BadgeRouter />} />
+                            <Route path='/menu/*' element={<MenuRouter />} />
                         </Routes>
                     </Row>
                 </Col>
